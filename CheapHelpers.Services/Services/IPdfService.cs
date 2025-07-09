2cfcec749b34c4fba93810dfd6a0a7a1894aa047
CheapNud <brechtvandeninden@hotmail.com>
2025-07-10 01:12:06 +0200

    introduced pdf service

--- CheapHelpers.Services/Services/IPdfService.cs
+++ CheapHelpers.Services/Services/IPdfService.cs
@@ -8,6 +8,7 @@ namespace CheapHelpers.Services
     {
         void Optimize(string source, string destination);
         void Optimize(Stream source, Stream destination);
-
+        Task<PdfOptimizationResult> OptimizeAsync(string source, string destination, PdfOptimizationLevel level = PdfOptimizationLevel.Balanced);
+        Task<PdfOptimizationResult> OptimizeAsync(Stream source, Stream destination, PdfOptimizationLevel level = PdfOptimizationLevel.Balanced);
     }
 }

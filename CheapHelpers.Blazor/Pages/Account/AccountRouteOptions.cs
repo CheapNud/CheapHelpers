968609beb06f8551938b5c739ce19a5d411e86b9
CheapNud <brechtvandeninden@hotmail.com>
2026-03-29 16:11:26 +0200

    opus

--- CheapHelpers.Blazor/Pages/Account/AccountRouteOptions.cs
+++ CheapHelpers.Blazor/Pages/Account/AccountRouteOptions.cs
@@ -17,10 +17,4 @@ public class AccountRouteOptions
     public string LoginRoute { get; set; } = "/Account/Login/";
     public string LockoutRoute { get; set; } = "/Account/Lockout/";
     public string EnableAuthenticatorRoute { get; set; } = "/Account/EnableAuthenticator";
-
-    /// <summary>
-    /// Whether to automatically map the account controller endpoints via MapControllers().
-    /// Set to false if the consumer registers their own derived controller.
-    /// </summary>
-    public bool AutoMapEndpoints { get; set; } = true;
 }
